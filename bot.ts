b97242ebba2bdd1bfc4428f8a85e38b3b70520b6
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-07-26 22:18:52 +0530

    send a msg if user is not a member of chat

--- bot.ts
+++ bot.ts
@@ -1,11 +1,11 @@
-import { Bot, Context } from './deps.deno.ts';
+import { Bot, Context, InlineKeyboard } from './deps.deno.ts';
 import { botToken, joinCheckId, joinCheckEnabled } from './config.ts';
 
 export const bot = new Bot(botToken);
 
 // custom filters
 const pm = bot.filter((ctx: Context) => ctx.chat?.type === 'private');
-const joinCheck = bot.filter((ctx: Context) => {
+const joinCheck = bot.filter(async (ctx: Context) => {
   // if joincheck is not enabled, return true
   if (!joinCheckEnabled) {
     return true;
@@ -13,23 +13,42 @@ const joinCheck = bot.filter((ctx: Context) => {
 
   // store const's
   const userId = Number(ctx.from?.id);
-  const member = ctx.api.getChatMember(joinCheckId, userId).then((member) => {
-    return (
-      member.status === 'member' ||
-      member.status === 'creator' ||
-      member.status === 'administrator'
-    );
-  });
+  const member = await ctx.api
+    .getChatMember(joinCheckId, userId)
+    .then((member) => {
+      return (
+        member.status === 'member' ||
+        member.status === 'creator' ||
+        member.status === 'administrator'
+      );
+    });
+
+  // if user is not a member, also send a message to ask user to join channel
+  if (!member) {
+    const joinChannel = 'DivideProjects';
+    await ctx.reply(`You have to join my channel @${joinChannel} to use me!`, {
+      reply_markup: new InlineKeyboard().url(
+        'Join Channel',
+        `https://t.me/${joinChannel}`,
+      ),
+    });
+  }
+
   return member;
 });
 
-pm.command('start', (ctx: Context) => ctx.reply('Welcome! Up and running.'));
-pm.command('ping', (ctx: Context) =>
-  ctx.reply(`Pong! ${new Date()} ${Date.now()}`),
+pm.command(
+  'start',
+  async (ctx: Context) => await ctx.reply('Welcome! Up and running.'),
 );
-pm.on([':media', ':file']).on(':forward_date', (ctx: Context) =>
-  ctx.copyMessage(Number(ctx.chat?.id)),
+pm.command(
+  'ping',
+  async (ctx: Context) => await ctx.reply(`Pong! ${new Date()} ${Date.now()}`),
 );
-joinCheck.command('works', (ctx: Context) => {
-  ctx.reply('Works!');
+pm.on([':media', ':file']).on(
+  ':forward_date',
+  async (ctx: Context) => await ctx.copyMessage(Number(ctx.chat?.id)),
+);
+joinCheck.command('works', async (ctx: Context) => {
+  await ctx.reply('Works!');
 });
