9962105c904b2059821ac36c6d48cabb09a85e19
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-07-26 10:03:02 +0530

    Add inital bot

---
+++ server.ts
@@ -0,0 +1,21 @@
+import { serve } from 'https://deno.land/x/sift@0.5.0/mod.ts';
+import { bot } from './bot.ts';
+import { webhookCallback } from './deps.deno.ts';
+
+const handleUpdate = webhookCallback(bot, 'std/http');
+
+serve({
+  ['/' + Deno.env.get('TOKEN')]: async (req) => {
+    if (req.method == 'POST') {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    return new Response();
+  },
+  '/': () => {
+    return new Response(JSON.stringify({ alive: true }, null, 2));
+  },
+});
