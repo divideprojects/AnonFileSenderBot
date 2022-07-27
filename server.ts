0590c47f60284e52b904dcfb9862caf4e3fa6ea5
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-07-27 13:49:50 +0530

    Count number of updates

--- server.ts
+++ server.ts
@@ -1,14 +1,16 @@
-import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
-import { bot } from "./bot.ts";
-import { botToken } from "./config.ts";
-import { webhookCallback } from "./deps.deno.ts";
+import { serve } from 'https://deno.land/x/sift@0.5.0/mod.ts';
+import { bot } from './bot.ts';
+import { botToken } from './config.ts';
+import { webhookCallback } from './deps.deno.ts';
 
-const handleUpdate = webhookCallback(bot, "std/http");
+const handleUpdate = webhookCallback(bot, 'std/http');
+let updates = 0;
 
 serve({
-  ["/" + botToken]: async (req) => {
-    if (req.method == "POST") {
+  ['/' + botToken]: async (req) => {
+    if (req.method == 'POST') {
       try {
+        updates++;
         return await handleUpdate(req);
       } catch (err) {
         console.error(err);
@@ -16,7 +18,9 @@ serve({
     }
     return new Response();
   },
-  "/": () => {
-    return new Response(JSON.stringify({ alive: true }, null, 2));
+  '/': () => {
+    return new Response(
+      JSON.stringify({ alive: true, updates: updates }, null, 2),
+    );
   },
 });
